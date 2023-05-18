9f232f993d26e1c4e7b641a7d07a22f42176fb05
bbnkmp <bbnkmp@mail.de>
2023-05-18 13:23:51 +0200

    include powMCTBinCount

--- R/powMCT.R
+++ R/powMCT.R
@@ -113,3 +113,152 @@ powMCT <- function(contMat, alpha = 0.025, altModels,
 ##   attributes(x)[2:5] <- NULL
 ##   print(x)
 ## }
+
+#' Function for power calculation for the multiple contrast test for
+#' binary data (candidate models formulated on logit link scale) and
+#' count data (negative binomial distribution with log link is
+#' assumed; candidate models formulated on log mean scale)- Note this
+#' function is somewhat limited (e.g. only one-sided testing allowed)
+#' and not user-friendly (e.g. cannot hand over Mods objects). It is
+#' not exported.
+#'
+#' @param n Vector of sample sizes per dose group
+#' @param doses
+#' @param candModList List containing the candidate models
+#' @param respModList List containing the response models to assume
+#' @param placEffu placebo effect on untransformed scale
+#' @param maxEffu maximum treatment effect vs placebo on untransformed
+#'   scale
+#' @param type either "binary_logit" or "negative_binomial"
+#' @param option whether the optimal contrast should be recalculated
+#'   for each assumed true model (option "A") or not option "B"
+#' @param alpha Significance level (one-sided testing is assumed)
+#' @param theta Overdispersion parameter required for the negative
+#'   binomial model. Parameterization of negative binomial:
+#'   E(Y)=mu, Var(Y)=mu*(1+mu/theta) 
+#' @param control Control parameter for mvtnorm::qmvt, mvtnorm::pmvt
+#'   (see ?mvtnorm.control)
+#' @param contMat Contrast matrix (if non-model-based contrasts should
+#'   be used)
+#' @param addArgs additional arguments for betaMod and linlog model
+#'   (passed to Mods function)
+#' 
+#' @return Vector of calculated power values
+#' @examples
+#'mvt_control <- mvtnorm.control()
+#'candModList <- list(emax = c(0.25, 1), sigEmax = rbind(c(1, 3), c(2.5, 4)), betaMod = c(1.1, 1.1))
+#'powMCTBinCount(rep(20,5), doses = c(0, 0.5, 1.5, 2.5, 4),
+#'               candModList=candModList, respModList=candModList,
+#'               placEffu = 0.1, maxEffu = 0.25, 
+#'               type = "binary_logit", option = "A",
+#'               alpha = 0.1, theta, control = mvt_control,
+#'               addArgs = list(scal = 4.8))
+powMCTBinCount <- function(n, doses, candModList, respModList,
+                           placEffu, maxEffu, 
+                           type = c("binary_logit", "negative_binomial"),
+                           option = c("A", "B"),
+                           alpha, theta, control, contMat = NULL, addArgs){
+  type <- match.arg(type)
+  option <- match.arg(option)
+  stopifnot(length(doses) == length(n))
+  if(type == "binary_logit"){ 
+    logit <- function(p)
+      log(p/(1-p))
+    trafo <- logit
+  }
+  if(type == "negative_binomial"){
+    if(is.null(theta))
+      stop("need argument theta for type = \"negative_binomial\"")
+    trafo <- log
+  }
+  placEff_tr <- trafo(placEffu)
+  maxEff_tr <- trafo(placEffu+maxEffu)-placEff_tr
+  
+  if(is.null(contMat)){
+    mods <- do.call(Mods, append(candModList,
+                                 list(placEff = placEff_tr, maxEff = maxEff_tr,
+                                      doses = doses, addArgs=addArgs)))
+    
+    if(option == "B"){ # opt. contrasts not recalculated only calculated here
+      contMat <- optContr(mods, w=n) # assume diagonal cov matrix with entries 1/n_i
+      cm <- contMat$contMat
+    }
+  } else {
+    cm <- contMat
+  }
+
+  ## calculate correlation matrix under null-hypothesis
+  resp_null <- do.call(Mods, append(respModList,
+                                    list(placEff = placEff_tr, maxEff = 0,
+                                         doses = doses, addArgs=addArgs)))
+  mu_null <- getResp(resp_null)[, 1, drop=FALSE] # under null all models are the same
+  v_null <- getVarBinCount(mu_null, type, theta)
+  S_null <- diag(as.vector(v_null)/n)
+  
+  resp_mods <- do.call(Mods, append(respModList,
+                                    list(placEff = placEff_tr, maxEff = maxEff_tr,
+                                         doses = doses, addArgs=addArgs)))
+  resp <- getResp(resp_mods)
+  nMod <- ncol(resp)
+  pow <- numeric(nMod)
+  for(i in 1:nMod){
+    mu_vec <- resp[,i, drop=FALSE] # column i contains true response vector
+    ## calculate covariance matrix
+    v <- getVarBinCount(mu_vec, type, theta)
+    S <- diag(as.vector(v)/n)
+    if(option == "A"){ # (re)calculate optimal contrasts based on S
+      contMat <- optContr(mods, S=S)
+      cm <- contMat$contMat
+    }
+    
+    ## calculate non-centrality parameter
+    delta <- t(cm) %*% mu_vec
+    covMat <- t(cm) %*% S %*% cm
+    den <- sqrt(diag(covMat))
+    delta <- delta/den
+    corMat <- cov2cor(covMat)
+    if(option == "A" | (option == "B" & i == 1)){ # for option B critV does not change
+      if(option == "A"){ # ADDPLAN-DF appears to use corMat not corMat_null
+        corMat_null <- corMat # for consistency do the same
+      }
+      if(option == "B"){
+        covMat_null <- t(cm) %*% S_null %*% cm
+        corMat_null <- cov2cor(covMat_null)
+      }
+      ## calculate critical value (df=0 corresponds to infinite degrees of freedom -> MVN distribution)
+      qmvtCall <- c(list(1 - alpha, tail = "lower.tail", df = 0, corr = corMat_null,
+                         algorithm = control, interval = control$interval))
+      critV <- do.call("qmvt", qmvtCall)$quantile
+    }
+    ## calculate power
+    lower <- rep(-Inf, nMod)
+    upper <- rep(critV, nMod)
+    control$interval <- NULL
+    pmvtCall <- c(list(lower, upper, df = 0, corr = corMat,
+                       delta = as.vector(delta), algorithm = control))
+    pow[i] <- as.vector(1 - do.call("pmvt", pmvtCall))
+  }
+  names(pow) <- colnames(resp)
+  pow
+}
+
+#' Function to calculate unit variances for mu_hat for binary (with
+#' logit link) and negative-binomial (with log link). Resulting
+#' variances need to be multiplied with factor 1/n.
+#' @param muVec Vector of group means on transformed scale (logit
+#'   scale for binary data, log scale for negative binomial)
+#' @param type either "binary_logit" or "negative_binomial"
+#' @param theta Overdispersion parameter required for the negative
+#'   binomial model. Parameterization of negative binomial: E(Y)=mu,
+#'   Var(Y)=mu*(1+mu/theta)
+#' @return Vector of variances
+getVarBinCount <- function(muVec, type, theta){
+  if(type == "binary_logit"){
+    inv_logit <- function(x)
+      1/(1+exp(-x))
+    p <- inv_logit(muVec) # mean responses on probability scale
+    return(1 / (p * (1 - p)))
+  }
+  if(type == "negative_binomial")
+    return((theta+exp(muVec))/(theta*exp(muVec)))
+}
---
+++ tests/testthat/test-powMCTBinCount.R
@@ -0,0 +1,93 @@
+context("power calculation binary and count data")
+
+## general options
+mvt_control <- mvtnorm.control(maxpts=1e5, abseps = 0.0001)
+
+## example (binary data)
+candModList <- list(emax = c(0.25, 1), sigEmax = rbind(c(1, 3), c(2.5, 4)), betaMod = c(1.1, 1.1))
+
+powA <- powMCTBinCount(rep(20,5), doses = c(0, 0.5, 1.5, 2.5, 4),
+                       candModList=candModList, respModList=candModList,
+                       placEffu = 0.1, maxEffu = 0.25, 
+                       type = "binary_logit", option = "A",
+                       alpha = 0.1, theta, control = mvt_control,
+                       addArgs = list(scal = 4.8))
+## externally calculated result in order Emax1,  Emax2,  sigEmax1, sigEmax2, betaMod
+extern_pow <- c(0.6792, 0.7365, 0.825, 0.8007, 0.7139)
+expect_equal(unname(powA), extern_pow, tolerance = 0.001)
+
+## just test whether code fails (for testing option = "B" see further below)
+powB <- powMCTBinCount(rep(20,5),doses = c(0, 0.5, 1.5, 2.5, 4),
+                       candModList=candModList, respModList=candModList,
+                       placEffu = 0.1, maxEffu = 0.25, 
+                       type = "binary_logit", option = "B",
+                       alpha = 0.1, theta, control = mvt_control,
+                       addArgs = list(scal = 4.8))
+
+## example (negative binomial data)
+candModList <- list(emax = c(0.1, 0.5, 1, 2), sigEmax = rbind(c(1, 3), c(3, 3)),
+                    betaMod = c(0.37, 0.74))
+powA <- powMCTBinCount(c(100,50,50,100,100), doses = c(0, 0.5, 2, 4, 8), 
+                       candModList=candModList, respModList=candModList,
+                       placEffu = 0.6, maxEffu = -0.3, 
+                       type = "negative_binomial", option = "A",
+                       alpha = 0.025, theta = 1.25, control = mvt_control,
+                       addArgs = list(scal = 9.6))
+## externally calculated result in order Emax1,  Emax2,  Emax3,  Emax4, sigEmax1, sigEmax2, betaMod
+extern_pow <- c(0.9035, 0.8816, 0.8691, 0.8518, 0.9264,  0.8518, 0.7913)
+expect_equal(unname(powA), extern_pow, tolerance = 0.001)
+
+## just test whether code fails (for testing option = "B" see further below)
+powB <- powMCTBinCount(c(100,50,50,100,100), doses = c(0, 0.5, 2, 4, 8), 
+                       candModList=candModList, respModList=candModList,
+                       placEffu = 0.6, maxEffu = -0.3, 
+                       type = "negative_binomial", option = "B",
+                       alpha = 0.025, theta = 1.25, control = mvt_control,
+                       addArgs = list(scal = 9.6))
+
+
+## tests for option = "B"
+## cannot validate against external results, so validate against manually calculated result
+cVal <- qnorm(1-0.05)
+n <- 50
+
+## binary case
+logit <- function(x)
+  log(x/(1-x))
+placEffu <- 0.4
+maxEffu <- 0.3
+p1 <- placEffu+maxEffu
+p0 <- placEffu
+nc_num <- logit(p1)-logit(p0)
+nc_den <- sqrt(1/(n*p0*(1-p0))+1/(n*p1*(1-p1)))
+delta <- nc_num/nc_den
+power_bin <- 1-pnorm(cVal, delta, 1)
+## compare against powMCTBinCount
+candModList <- list(linear = NULL)
+powB <- DoseFinding:::powMCTBinCount(c(n,n), doses = c(0, 1), 
+                       candModList=candModList, respModList=candModList,
+                       placEffu = placEffu, maxEffu = maxEffu, 
+                       type = "binary_logit", option = "B",
+                       alpha = 0.05, control = mvt_control,
+                       addArgs = list(scal = 1))
+expect_equal(unname(powB), power_bin, tolerance = 0.000001)
+
+## negative binomial case
+placEffu <- 1
+maxEffu <- -0.5
+theta <- 2
+r0 <- placEffu
+r1 <- placEffu + maxEffu
+nc_num <- -1*(log(r1)-log(r0)) # multiply with -1 (decreasing)
+v <- (theta+c(r0, r1))/(theta*c(r0, r1))
+nc_den <- sqrt(sum(v/c(n,n)))
+delta <- nc_num/nc_den
+power_nb <- 1-pnorm(cVal, delta, 1)
+
+powB <- DoseFinding:::powMCTBinCount(c(n,n), doses = c(0, 1), 
+                       candModList=candModList, respModList=candModList,
+                       placEffu = placEffu, maxEffu = maxEffu, 
+                       type = "negative_binomial", option = "B",
+                       alpha = 0.05, theta = theta, control = mvt_control,
+                       addArgs = list(scal = 1))
+expect_equal(unname(powB), power_nb, tolerance = 0.000001)

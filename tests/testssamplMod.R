b73b130f6916f9b253bcdfd0a68c6f68a1b0b6a8
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-05-07 20:42:29 +0800

    activate again testssamplMod.R because that was not the problem

--- tests/testssamplMod.R
+++ tests/testssamplMod.R
@@ -3,8 +3,6 @@ require("DoseFinding")
 ## test Bayesian fitting
 ## compare bFitMod on example data set with jags
 
-if (!testthat:::on_ci()) {
-
 data(biom)
 
 ## (i) fit biom data
@@ -121,4 +119,3 @@ td1 <- TD(gsample, 0.3)
 td2 <- TD(gsample, 0.3, TDtype="d", doses = seq(0,4,length=101))
 ed1 <- ED(gsample, 0.8)
 ed2 <- ED(gsample, 0.8, EDtype="d", doses = seq(0,4,length=101))
-}
